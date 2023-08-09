b546af46c61e225d84228c06255d4621fb96f304
greenleaf-glitch <133708156+greenleaf-glitch@users.noreply.github.com>
2023-08-09 23:45:24 +0300

    fin v1.0

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryUL: document.querySelector(".gallery"),
+};
+
+const markup = galleryItems
+  .map((item) => {
+    return `<li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>`;
+  })
+  .join("");
+
+refs.galleryUL.insertAdjacentHTML("afterbegin", markup);
+
+refs.galleryUL.addEventListener("click", onImgClick);
+
+function onImgClick(event) {
+  event.preventDefault();
+
+  if (event.target === event.currentTarget) return;
+
+  const bigImgURL = event.target.dataset.source;
+  const modalWindow = basicLightbox.create(`
+    <div class="modal">
+        <img
+          src="${bigImgURL}"
+        />
+    </div>
+`);
+
+  modalWindow.show();
+
+  refs.galleryUL.addEventListener("keydown", (event) => {
+    if (event.key !== "Escape") return;
+    modalWindow.close();
+    refs.galleryUL.removeEventListener("keydown", {});
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryUL: document.querySelector(".gallery"),
+};
+
+const markup = galleryItems
+  .map(({ original, preview, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image"
+      src="${preview}"
+      alt="${description}" />
+   </a>
+   </li>`;
+  })
+  .join("");
+
+refs.galleryUL.insertAdjacentHTML("afterbegin", markup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  loop: true,
+});
